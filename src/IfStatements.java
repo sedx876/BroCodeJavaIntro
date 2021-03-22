e817c5f28bb8327161466fa12c691ce6321d9435
sedx876 <swatkins876@gmail.com>
2021-03-22 05:29:12 -0400

    Code

--- src/IfStatements.java
+++ src/IfStatements.java
@@ -2,7 +2,21 @@
 public class IfStatements {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int age = 75;
+		
+		if(age >= 75) {
+			System.out.println("OK Boomer!");
+		}
+		else if(age >= 18) {
+			System.out.println("You are an adult");
+		}
+		else if(age >= 13) {
+			System.out.println("You are a teenager");
+		}
+		else {
+			System.out.println("You are not an adult");
+		}
 
 	}
 
