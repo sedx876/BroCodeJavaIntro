97a9401842a172a4dedd4ec55547e154abd341ba
sedx876 <swatkins876@gmail.com>
2021-03-31 08:51:18 -0400

    toString

--- src/ToString.java
+++ src/ToString.java
@@ -1,4 +1,17 @@
 
 public class ToString {
 
+	public static void main(String[] args) {
+		//toString() = special method that all objects inherit
+		//				that returns a string that textually represents an object
+		//				can be used implicitly and explicitly
+		
+		ToStringCar car = new ToStringCar();
+		System.out.println(car.make);
+		System.out.println(car.model);
+		System.out.println(car.color);
+		System.out.println(car.year);
+		System.out.println(car.toString());
+		System.out.println(car);
+	}
 }
--- src/ToStringCar.java
+++ src/ToStringCar.java
@@ -1,4 +1,13 @@
 
 public class ToStringCar {
 
+	String make = "Ford";
+	String model = "Mustang";
+	String color = "Red";
+	int year = 2021;
+	
+	public String toString() {
+		String carString = make + "\t" +model+ "\t" +"\t" +color+ "\t" +year;
+		return carString;
+	}
 }
